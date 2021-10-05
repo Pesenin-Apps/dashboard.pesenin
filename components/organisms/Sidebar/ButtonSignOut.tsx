88a5a9b8b650654439fb5cbb4683d2b718927557
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-10-05 12:13:47 +0800

    add confirm dialog before signOut

--- components/organisms/Sidebar/ButtonSignOut.tsx
+++ components/organisms/Sidebar/ButtonSignOut.tsx
@@ -1,4 +1,5 @@
 import Cookies from 'js-cookie';
+import Swal from 'sweetalert2';
 import { useRouter } from 'next/router';
 import { toast } from 'react-toastify';
 import { signOut } from '../../../services/auth';
@@ -6,15 +7,24 @@ import { signOut } from '../../../services/auth';
 export default function ButtonSignOut() {
   const router = useRouter();
   const onSignOut = async () => {
-    const response = await signOut();
-    console.log(response);
-    if (response.error) {
-      toast.error(response.message);
-    } else {
-      Cookies.remove('token');
-      toast.success('Sign Out, Berhasil!');
-      router.push('/');
-    }
+    Swal.fire({
+      title: 'Anda yakin ingin Keluar?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#ffa700',
+      confirmButtonText: 'Ya, Keluar!',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const response = await signOut();
+        if (response.error) {
+          toast.error(response.message);
+        } else {
+          Cookies.remove('token');
+          toast.success('Sign Out, Berhasil!');
+          router.push('/');
+        }
+      }
+    });
   };
 
   return (
