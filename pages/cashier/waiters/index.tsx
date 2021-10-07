8343ae3c28ca39f067a996db333b1c063325d248
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-10-08 00:22:37 +0800

    add serverSideProps in waiters page

--- pages/cashier/waiters/index.tsx
+++ pages/cashier/waiters/index.tsx
@@ -1,6 +1,8 @@
+import jwtDecode from 'jwt-decode';
 import ContentWaiter from '../../../components/organisms/ContentWaiter';
 import Navbar from '../../../components/organisms/Navbar';
 import Sidebar from '../../../components/organisms/Sidebar';
+import { UserTypes } from '../../../services/data-types';
 
 export default function Waiters() {
   return (
@@ -14,4 +16,41 @@ export default function Waiters() {
       </main>
     </section>
   );
+}
+
+interface GetServerSideProps {
+  req: {
+    cookies: {
+      token: string;
+    };
+  };
+}
+
+export async function getServerSideProps({ req }: GetServerSideProps) {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  const jwtToken = Buffer.from(token, 'base64').toString('ascii');
+  const payload: UserTypes = jwtDecode(jwtToken);
+  const userFromPayload: UserTypes = payload;
+
+  if (userFromPayload.role !== 'cashier') {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
 }
